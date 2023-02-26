b246168da409acb02aec298a89bac6c72575b132
Chris Zetter <zetter@users.noreply.github.com>
2023-02-26 16:10:55 +0000

    fix env var

--- sidekiq_settings.rb
+++ sidekiq_settings.rb
@@ -3,7 +3,7 @@ require 'redis-namespace'
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = {db: 1, url: ENV['MASTODON_ACCOUNT_TOKEN'] }
+  config.redis = {db: 1, url: ENV['REDIS_URL'] }
 end
  
 Sidekiq.configure_client do |config|
